bb193c70454e2e09bab36ed195195f668aa80ef6
jalmansa <lordneru@gmail.com>
2019-04-10 17:45:11 +0200

    Finishing project

--- limits-service/src/main/java/io/almansa/microservices/limits/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/io/almansa/microservices/limits/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.almansa.microservices.limits.limitsservice.bean.LimitConfiguration;
 
 @RestController
@@ -15,5 +17,16 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		//some code that throw an exception
+		throw new RuntimeException("Not Available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
 	}
 }
